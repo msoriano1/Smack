6e34b6e67650df6b30b19d44d5ac3d3e2d0c99f7
msoriano1 <matthew.soriano@obf.ateneo.edu>
2019-07-22 22:02:35 +0800

    lesson-04

--- app/src/main/java/com/mattdog/smack/LoginActivity.kt
+++ app/src/main/java/com/mattdog/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.mattdog.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,7 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view:View){
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 
     fun loginLoginBtnClicked(view: View){
